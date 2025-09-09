1a983006772dcdcb973b609e069f93a53f237124
cpinay <c.pinay@monlivretc.com>
2025-09-09 10:59:40 +0200

    Initial commit project C#

---
+++ xUnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+using ModernSoftwareApp;
+
+namespace xUnitTestProject
+{
+
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Add(2, 3));
+        }
+
+    }
+}
